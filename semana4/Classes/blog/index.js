94bff154c273ca24e0905240687549a4679d683b
Danilo Mourelle <danilomourelle@outlook.com>
2020-02-05 17:05:05 -0300

    Desafio 1 - coloquei a imagem, só não deu tempo de tratar pra ser imagem e ordenar e formatar layout

--- semana4/Classes/blog/index.js
+++ semana4/Classes/blog/index.js
@@ -1,9 +1,10 @@
 class Post {
-  constructor(autor, titulo, tema, conteudo) {
+  constructor(autor, titulo, tema, conteudo, link) {
     this.autor = autor
     this.titulo = titulo
     this.tema = tema
     this.conteudo = conteudo
+    this.imagem = link
   }
 }
 
@@ -17,16 +18,27 @@ function criarPost() {
   document.getElementById("tema").value = ""
   let novoConteudo = document.getElementById("conteudo").value
   document.getElementById("conteudo").value = ""
-  let novoPost = new Post(novoAutor, novoTitulo, novoTema, novoConteudo)
+  let novaImagem = document.getElementById("imagem").value
+  if (novaImagem === ""){
+    novaImagem = null
+  }
+  document.getElementById("imagem").value = ""
+  let novoPost = new Post(novoAutor, novoTitulo, novoTema, novoConteudo, novaImagem)
 
-  
-  publicaPost(novoAutor, novoTitulo, novoTema, novoConteudo)
+  publicaPost(novoAutor, novoTitulo, novoTema, novoConteudo, novaImagem)
   todosOsPosts.push(novoPost)
   console.log(todosOsPosts) 
 }
 
-function publicaPost(autor, titulo, tema, conteudo){
-  document.getElementById("post").innerHTML += "<div><h4>" + tema + "</h4><h2>" + titulo + "</h2><p>" + conteudo + "</p><h4>" + autor + "</h4></div>"
+function publicaPost(autor, titulo, tema, conteudo, imagem){
+  let postagem = "<div><h4>" + tema + "</h4><h2>" + titulo + "</h2><p>" + conteudo + "</p><h4>" + autor + "</h4>"
+  if (imagem!==null) {
+    postagem+= "<img src=" + imagem + "></div>"
+  }
+  else{
+    postagem+= "</div>"
+  }
+  document.getElementById("post").innerHTML += postagem
 }
 
 
