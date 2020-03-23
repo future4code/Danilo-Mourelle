c975379c0ad98435f8a326fd84a9d1ea80c4932e
Danilo Mourelle <danilomourelle@outlook.com>
2020-03-23 17:25:21 -0300

    Parte de segunda sem a integração de um dos endpoints

---
+++ semana10/projeto-semana/src/Actions/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const baseUrl = 'https://us-central1-missao-newton.cloudfunctions.net/futureX/'
+const user = 'mourelle'
+
+//***** ASSÍNCRONAS*****//
+export const getTripsList = () => async (dispatch) => {
+  try{
+    const response = await axios.get('https://us-central1-missao-newton.cloudfunctions.net/futureX/mourelle/trips')
+    console.log('passei aqui')
+    dispatch(setTripsList(response.data.trips))
+  } catch (error) {
+    console.log('passei aqui no erro')
+  }
+}
+
+//*****SÍNCRONAS*****//
+export const setTripsList = (list) => ({
+  type: 'SET_TRIPS_LIST',
+  payload: {
+    list,
+  }
+})
