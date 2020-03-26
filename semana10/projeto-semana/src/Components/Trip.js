5475e04296983641e49b9c6090db9e64b718c390
Danilo Mourelle <danilomourelle@outlook.com>
2020-03-26 12:49:12 -0300

    Layout da página de lista de viagens e alterações no componente Trip.js

--- semana10/projeto-semana/src/Components/Trip.js
+++ semana10/projeto-semana/src/Components/Trip.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from "@material-ui/core/Button";
+
+const Wrapper = styled.div`
+  width: 40%;
+  height:250px;
+  border: solid 2px orange;
+  border-radius: 5px;
+  padding: 20px;
+  margin:25px;
+  display:flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items:center;
+`
+const DetailField = styled.p`
+  margin:0;
+  margin-bottom:10px;
+  width: 40%;
+`
+const DetailsWrapper = styled.span`
+  width:100%;
+  height:70%;
+  display:flex;
+  flex-wrap:wrap;
+  flex-direction:column;
+`
+const ButtonWrapper = styled.span`
+  width: 100%;
+  text-align:center;
+`
 
 function Trip(props) {
   return (
-    <div>
-      <p><strong>Trip Id:</strong></p>
-      <p>{props.trip.id}</p>
-      <p><strong>Trip Name:</strong></p>
-      <p>{props.trip.name}</p>
-      <p><strong>Trip Planet:</strong></p>
-      <p>{props.trip.planet}</p>
-      <p><strong>Trip Date:</strong></p>
-      <p>{props.trip.date}</p>
-      <p><strong>Trip Description:</strong></p>
-      <p>{props.trip.description}</p>
-      <p><strong>Trip Duration:</strong></p>
-      <p>{props.trip.durationInDays}</p>
-      <hr />
-    </div>
+    <Wrapper>
+      <DetailsWrapper>
+        <DetailField><strong>Nome do Evento: </strong>{props.trip.name}</DetailField>
+        <DetailField><strong>Planeta: </strong>{props.trip.planet}</DetailField>
+        <DetailField><strong>Data: </strong>{props.trip.date}</DetailField>
+        <DetailField><strong>Descrição: </strong>{props.trip.description}</DetailField>
+        <DetailField><strong>Duração do Evento: </strong>{props.trip.durationInDays} dias</DetailField>
+
+      </DetailsWrapper>
+      <ButtonWrapper>
+        <Button
+          color='primary'
+          variant="contained"
+          type='submit' >Detalhes</Button>
+      </ButtonWrapper>
+
+    </Wrapper>
   )
 }
 
--- semana10/projeto-semana/src/containers/ListTripsPage/index.js
+++ semana10/projeto-semana/src/containers/ListTripsPage/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
-
 import { connect } from "react-redux";
-import Trip from '../../Components/Trip';
 import { getTripsList } from "../../Actions"
+
+import Trip from '../../Components/Trip';
+import ButtonAppBar from '../../Components/AppBar'
+import Title from '../../Components/Title'
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items:flex-start;
+`
+const List = styled.div`
+  width:80%;
+  display: flex;
+  flex-wrap:wrap;
+  justify-content:space-between;
+`
+
 
 
 class ListTripPages extends React.Component {
@@ -15,11 +33,15 @@ class ListTripPages extends React.Component {
     const { tripList } = this.props
     console.log(tripList)
     return (
-      <div>
-        {tripList.map((trip, index, tripList) => (
-          <Trip key={index} trip={trip} />
-        ))}
-      </div>
+      <Wrapper>
+        <ButtonAppBar btnText='LOGOUT' />
+        <Title> Lista com as viagens cadastradas </Title>
+        <List>
+          {tripList.map((trip, index, tripList) => (
+            <Trip key={index} trip={trip} />
+          ))}
+        </List>
+      </Wrapper>
     )
   }
 }
