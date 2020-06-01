d9f059961e2ca88c5a3370040e1579c89ec1fc78
Danilo Mourelle <danilomourelle@outlook.com>
2020-06-01 17:01:50 -0300

    Exercício 1, 2, 3 e 4

---
+++ semana20/testes-automatizados/tests/Exercício 4/verifyAge.test.ts
@@ -0,0 +1,73 @@
+import { Casino, User, Result, NACIONALITY, LOCATION } from "../../src/Exercício 3/interfacerEenums"
+import { verifyAge } from "../../src/Exercício 3/verifyAge"
+
+describe("Testing function Verify Error", () => {
+  test("Should accept a brazilian in a Brazil casino", () => {
+    const cassino: Casino = {
+      name: "BelaDonna",
+      location: LOCATION.BRAZIL
+    }
+    const user: User[] = [{
+      nacionality: NACIONALITY.BRAZILIAN,
+      name: "Rosana",
+      age: 20
+    }]
+
+    const result: Result = verifyAge(cassino, user)
+
+    expect(result.brazilians.allowed).toContain("Rosana")
+
+  })
+  test("Should accept a american in a Brazil casino", () => {
+    const cassino: Casino = {
+      name: "BelaDonna",
+      location: LOCATION.BRAZIL
+    }
+    const user: User[] = [{
+      nacionality: NACIONALITY.AMERICAN,
+      name: "Felipe",
+      age: 20
+    }]
+
+    const result: Result = verifyAge(cassino, user)
+
+    expect(result.americans.allowed).toContain("Felipe")
+
+  })
+  test("Should result for a EUA casino", () => {
+    const cassino: Casino = {
+      name: "Mirage",
+      location: LOCATION.EUA
+    }
+    const users: User[] = [
+      {
+        nacionality: NACIONALITY.BRAZILIAN,
+        name: "Danilo",
+        age: 19
+      },
+      {
+        nacionality: NACIONALITY.BRAZILIAN,
+        name: "Elo",
+        age: 19
+      },
+      {
+        nacionality: NACIONALITY.AMERICAN,
+        name: "Leo",
+        age: 19
+      },
+      {
+        nacionality: NACIONALITY.AMERICAN,
+        name: "Marina",
+        age: 19
+      },
+    ]
+    const result: Result = verifyAge(cassino, users)
+
+    expect(result.brazilians.unallowed).toContain("Danilo")
+    expect(result.brazilians.unallowed).toContain("Elo")
+    expect(result.brazilians.unallowed).toHaveLength(2)
+    expect(result.americans.unallowed).toContain("Leo")
+    expect(result.americans.unallowed).toContain("Marina")
+    expect(result.americans.unallowed).toHaveLength(2)
+  })
+})
